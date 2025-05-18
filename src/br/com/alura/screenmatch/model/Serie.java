ef801a4c8beb7b24cb4da2c9bc0ccf433dd36063
vlonerickk <richard.rcket@gmail.com>
2025-05-18 20:27:49 -0300

    Getters and Setters

---
+++ src/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.model;
+
+public class Serie extends Titulo{
+    private int temporada;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(int temporada) {
+        this.temporada = temporada;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporada * episodiosPorTemporada * minutosPorEpisodio;
+    }
+}
