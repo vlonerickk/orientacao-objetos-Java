5719818627c2ac71c6304451cd82e2024f593e01
vlonerickk <richard.rcket@gmail.com>
2025-05-18 20:27:18 -0300

    Calcular tempo para maratonar filme/série.

---
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.model.Filme;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    public void inclui(Filme f){
+        tempoTotal += f.getDuracaoEmMinutos();
+    }
+}
